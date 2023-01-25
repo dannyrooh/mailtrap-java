d2bb2b1439a4950e45befa9686ff9ab78ed0d1b9
Dannyroohm <dannyrooh@hotmail.com>
2023-01-25 20:26:16 -0300

    inicializado classe

---
+++ src/test/java/br/com/webmadria/sendmail/SendMailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webmadria.sendmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
